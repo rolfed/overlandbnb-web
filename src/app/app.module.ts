e9ba31847df01668e5a95f99f185977827424d9b
Ruben Piatnitsky <r.piatnitsky@gmail.com>
2020-03-28 22:36:06 -0700

    Clean up app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NavigationComponent } from './shared/component/navigation/navigation.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
-import { HTTP_INTERCEPTORS } from "@angular/common/http";
-import { JwtInterceptor } from "./shared/interceptor/jwt-interceptor";
+import { JwtInterceptor } from './shared/interceptor/jwt-interceptor';
 import { ErrorInterceptor } from './shared/interceptor/error-interceptor';
 
 @NgModule({
@@ -31,7 +31,7 @@ import { ErrorInterceptor } from './shared/interceptor/error-interceptor';
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
-  ], 
-  bootstrap: [AppComponent] 
-}) 
+  ],
+  bootstrap: [AppComponent]
+})
 export class AppModule { }
