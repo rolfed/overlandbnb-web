e98bce2e961c9c052210ebcf365d4e6f81707993
Ruben Piatnitsky <r.piatnitsky@gmail.com>
2020-03-28 22:00:45 -0700

    Fix unit test error in error interceptor

--- src/app/shared/interceptor/error-interceptor.spec.ts
+++ src/app/shared/interceptor/error-interceptor.spec.ts
@@ -1,7 +1,15 @@
 import { ErrorInterceptor } from './error-interceptor';
+import { AuthenticationService } from '../service/authentication.service';
 
 describe('ErrorInterceptor', () => {
+
+  let mockAuthService: jasmine.SpyObj<AuthenticationService>;
+
+  beforeEach(() => {
+    mockAuthService = jasmine.createSpyObj('AuthenticationService', ['']);
+  });
+
   it('should create an instance', () => {
-    expect(new ErrorInterceptor()).toBeTruthy();
+    expect(new ErrorInterceptor(mockAuthService)).toBeTruthy();
   });
 });
