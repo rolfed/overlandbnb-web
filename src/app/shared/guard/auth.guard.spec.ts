83ce671ad2452a7fa28e29b32e29d49256157936
Ruben Piatnitsky <r.piatnitsky@gmail.com>
2020-03-28 23:04:03 -0700

    Fix authguard unit tests

--- src/app/shared/guard/auth.guard.spec.ts
+++ src/app/shared/guard/auth.guard.spec.ts
@@ -1,10 +1,22 @@
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { AuthGuard } from './auth.guard';
+import { AuthenticationService } from '../service/authentication.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
 
 describe('AuthGuard', () => {
+  let mockAuthService: jasmine.SpyObj<AuthenticationService>;
+
   beforeEach(() => {
+    mockAuthService = jasmine.createSpyObj('AuthenticationService', ['']);
+
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule
+      ],
       providers: [AuthGuard]
     });
   });
