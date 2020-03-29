a2c0ab7b560f4827c89556b8937e958de9c0fcc8
Ruben Piatnitsky <r.piatnitsky@gmail.com>
2020-03-28 22:35:47 -0700

    Fix unit test error and lint errors

--- src/app/admin/user-edit/user-edit.component.spec.ts
+++ src/app/admin/user-edit/user-edit.component.spec.ts
@@ -1,14 +1,36 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserEditComponent } from './user-edit.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker/public_api';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/shared/service/user.service';
+
 
 describe('UserEditComponent', () => {
   let component: UserEditComponent;
   let fixture: ComponentFixture<UserEditComponent>;
+  let mockRouter: jasmine.SpyObj<Router>;
+  let mockUserService: jasmine.SpyObj<UserService>;
 
   beforeEach(async(() => {
+    mockRouter = jasmine.createSpyObj('Router', []);
+    mockUserService = jasmine.createSpyObj('UserService', []);
+
     TestBed.configureTestingModule({
-      declarations: [ UserEditComponent ]
+      declarations: [
+        UserEditComponent
+      ],
+      imports: [
+        ReactiveFormsModule,
+        RouterTestingModule,
+        BsDatepickerModule.forRoot()
+      ],
+      providers: [
+        { provide: Router, useValue: mockRouter },
+        { provide: UserService, useValue: mockUserService }
+      ]
     })
     .compileComponents();
   }));
--- src/app/admin/user-edit/user-edit.component.ts
+++ src/app/admin/user-edit/user-edit.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from "@angular/forms";
-import { UserService } from "../../shared/service/user.service";
-import { Router } from "@angular/router";
-import { User } from "../../shared/model/user";
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UserService } from '../../shared/service/user.service';
+import { Router } from '@angular/router';
+import { User } from '../../shared/model/user';
+
 
 @Component({
   selector: 'ovb-user-edit',
@@ -11,56 +12,20 @@ import { User } from "../../shared/model/user";
 })
 export class UserEditComponent implements OnInit {
   public user: User;
+  public isSubmitted: boolean;
 
-  constructor(
-    private userService: UserService,
-    private router: Router
-  ) {}
-
-  ngOnInit() {
-    if (!!this.userService.user) {
-      this.user = this.userService.user[0];
-      console.log('EDIT USER -- USER: ', this.user);
-      this.userEditFG.patchValue(this.user);
-    } else {
-      this.router.navigate(['/admin']).then(v => v);
-    }
-  }
-
-  public emailFC: FormControl = new FormControl(
-    '', [Validators.required]);
-
-  public firstNameFC: FormControl = new FormControl(
-    '', [Validators.required]);
-
-  public lastNameFC: FormControl = new FormControl(
-    '', [Validators.required]);
-
-  public dateOfBirthFC: FormControl = new FormControl(
-    '', [Validators.required]);
-
-  public phoneFC: FormControl = new FormControl(
-    '', [Validators.required]);
-
-  public address1FC: FormControl = new FormControl(
-    '', [Validators.required]);
-
+  public emailFC: FormControl = new FormControl('', [Validators.required]);
+  public firstNameFC: FormControl = new FormControl('', [Validators.required]);
+  public lastNameFC: FormControl = new FormControl('', [Validators.required]);
+  public dateOfBirthFC: FormControl = new FormControl('', [Validators.required]);
+  public phoneFC: FormControl = new FormControl('', [Validators.required]);
+  public address1FC: FormControl = new FormControl('', [Validators.required]);
   public address2FC: FormControl = new FormControl('', []);
-
-  public cityFC: FormControl = new FormControl(
-    '', [Validators.required]);
-
-  public stateFC: FormControl = new FormControl(
-    '', [Validators.required]);
-
-  public postalCodeFC: FormControl = new FormControl(
-    '', [Validators.required]);
-
-  public countryFC: FormControl = new FormControl(
-    '', [Validators.required]);
-
-  public updateAt: FormControl = new FormControl(
-    new Date().toISOString(), []);
+  public cityFC: FormControl = new FormControl('', [Validators.required]);
+  public stateFC: FormControl = new FormControl('', [Validators.required]);
+  public postalCodeFC: FormControl = new FormControl('', [Validators.required]);
+  public countryFC: FormControl = new FormControl('', [Validators.required]);
+  public updateAt: FormControl = new FormControl(new Date().toISOString(), []);
 
   public userEditFG: FormGroup = new FormGroup({
     email: this.emailFC,
@@ -73,19 +38,36 @@ export class UserEditComponent implements OnInit {
     city: this.cityFC,
     state: this.stateFC,
     postalCode: this.postalCodeFC,
-    country: this.countryFC, 
-    updateAt: this.updateAt 
+    country: this.countryFC,
+    updateAt: this.updateAt
   });
+
+  constructor(
+    private userService: UserService,
+    private router: Router
+    ) {}
+
+  ngOnInit() {
+    if (!!this.userService.user) {
+      this.user = this.userService.user[0];
+      console.log('EDIT USER -- USER: ', this.user);
+      this.userEditFG.patchValue(this.user);
+    } else {
+      this.router.navigate(['/admin']).then(v => v);
+    }
+  }
+
 
   public onSubmit() {
     if (this.userEditFG.valid) {
+      this.isSubmitted = true;
       this.userService.updateUserById(this.user).subscribe(
         response => {
           console.log('RESPONSE: ', response);
         }, err => {
           console.error('ERROR: ', err);
         }
-      )
+      );
     }
   }
 }
