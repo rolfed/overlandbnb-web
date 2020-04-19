6d69b9cb7b16d2b005d5d5b23fa4c4f48b6d276f
Ruben Piatnitsky <r.piatnitsky@gmail.com>
2020-04-19 01:04:18 -0700

    Add search input in search component

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, ViewChild, ElementRef } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { QUERY_PARAMS } from '../shared/constants/routing.constants';
+import { QUERY_PARAMS, OBNB_ROUTES } from '../shared/constants/routing.constants';
 
 
 @Component({
@@ -8,8 +8,9 @@ import { QUERY_PARAMS } from '../shared/constants/routing.constants';
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent implements OnInit, OnChanges {
 
+  @ViewChild('searchInput', {static: true}) searchInput: ElementRef;
   public query: string;
 
   constructor(private router: Router,
@@ -19,16 +20,29 @@ export class SearchComponent implements OnInit {
     this.initSearch();
   }
 
+  ngOnChanges() {
+    this.initSearch();
+  }
+
   private initSearch(): void {
     const params = this.activeRoute.snapshot.queryParams;
     this.query = params[QUERY_PARAMS.QUERY];
 
     if (!!this.query) {
       // TODO: get the results from BE
-      console.log('SEARCH FOR: ', this.query);
+      console.log('SEARCH FOR: ', this.query, this.searchInput);
+      this.searchInput.nativeElement.value = this.query;
+
     } else {
       // TODO: Init without results
     }
+  }
+
+  public search(query: string): void {
+    const params = { queryParams: {} };
+    params.queryParams[QUERY_PARAMS.QUERY] = query;
+
+    this.router.navigate([OBNB_ROUTES.SEARCH], params);
   }
 
 }
